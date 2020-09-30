084a505c4194fa5778790fcc77cffc16cb08352a
zaxer2 <william.johns3@gmail.com>
2020-09-30 02:39:24 -1000

    Merged all to master, added rudimentary login functionality

--- lib/FirebaseFunctions.dart
+++ lib/FirebaseFunctions.dart
@@ -3,6 +3,56 @@ import 'package:flutter/material.dart';
 // Import the firebase_core and cloud_firestore plugin
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:math';
+import 'dart:async';
+
+class fireFunctions {
+  /**
+   * RED ALERT RED ALERT
+   *
+   * THIS IS NOT HOW YOU SHOULD HANDLE USER LOGIN DATA
+   *
+   * This has been a practice exercise in asynchronicity and database manipulation. Will convert this database-based login system to Firebase Authserver ASAP.
+   * In the meantime: DO NOT SUBMIT REAL PASSWORDS. THEY ARE STORED AS PLAINTEXT
+   *
+   */
+  static Future<int> createLogin(user, pass) {
+    CollectionReference logins = FirebaseFirestore.instance.collection('Logins');
+    Completer<int> completer = new Completer<int>();
+    Future<QuerySnapshot> results = logins.where("username", isEqualTo: user).get();
+    results.then((value) {
+      if (value.size != 0) {
+        completer.complete(-1);
+        return;
+      }
+      Random rng = new Random();
+      int newID = rng.nextInt(10000000);
+      logins.add({
+        'username': user,
+        'password': pass,
+        'verif_ID': newID
+      });
+      completer.complete(newID);
+    });
+
+    return completer.future;
+  }
+  static Future<int> findLogin(String user, String pass) async { //Returns a future with the ID of the found user, or -1 if user not found
+    CollectionReference logins = FirebaseFirestore.instance.collection('Logins');
+    Completer<int> completer = new Completer<int>();
+    Future<QuerySnapshot> results = logins.where("username", isEqualTo: user).where("password", isEqualTo: pass).get();
+
+    results.then((value) {
+      if (value.size != 1) {
+        completer.complete(-1);
+        return;
+      }
+      int ID = value.docs[0].data()["verif_ID"];
+      completer.complete(ID);
+    });
+    return completer.future;
+  }
+}
 
 class AddUser extends StatelessWidget {
   final String name;
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
+
+  //sets title for AppBar
+  static const String _title = 'Bowfolios';
+
   // Create the initilization Future outside of `build`:
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
@@ -38,7 +42,21 @@ class MyApp extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           print("Firebase initialized");
           return MaterialApp(
-            title: 'Flutter Demo',
+            title: _title,
+
+            //body of the page
+            home: Scaffold(
+
+              //appBar with Bowfolios title
+                appBar: AppBar(
+                  title: const Text(_title),
+                  backgroundColor: Colors.green[800],),
+                body: Center(
+                  child: MyStatefulWidget(),
+
+
+                )
+            ),
             theme: ThemeData(
               // This is the theme of your application.
               //
@@ -55,7 +73,6 @@ class MyApp extends StatelessWidget {
               // closer together (more dense) than on mobile platforms.
               visualDensity: VisualDensity.adaptivePlatformDensity,
             ),
-            initialRoute: '/profiles',
             routes: {
               '/profiles': (context) => ProfilePage(),
               '/projects': (context) => ProjectPage(),
@@ -159,3 +176,159 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final emailHandler = TextEditingController();
+  final pwdHandler = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+
+      key: _formKey,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            //widget for email text field entry
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                controller: emailHandler,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                validator: (value) {
+
+                  //prompts user to input email if not entered
+                  if (value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+            ),
+
+            //widget for password text field entry. will update to hide password
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+
+              child: TextFormField(
+                controller: pwdHandler,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your password',
+                ),
+
+                validator: (value) {
+
+                  //prompts user to enter password if not entered
+                  if (value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+            ),
+
+            //submit button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100.0, vertical: 8),
+
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                    //entries are valid, check credentials
+                    Future<int> id = fireFunctions.findLogin(emailHandler.value.text, pwdHandler.value.text);
+                    id.then((value) {
+                      print(value);
+                      if (value != -1 && value != null) {
+                        Navigator.pushNamed(context, '/profiles');
+                      } else {
+                        showDialogue(context, "An error has occurred", "Your email or password may be incorrect");
+                      }
+                    });
+                  }
+                },
+                child: Center (child: Text('Submit'),),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100.0, vertical: 5),
+              child: RaisedButton(
+                onPressed: () {
+                  showDialogue(context, "TODO: this button", "Sorry, this button doesn't do anything");
+                },
+                child: Center (child: Text('Forgot password?'),),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100.0, vertical: 5),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                    // Process data.
+                    Future<int> id = fireFunctions.createLogin(emailHandler.value.text, pwdHandler.value.text);
+                    id.then((value) {
+                      print(value);
+                      if (value != -1 && value != null) {
+                        print("Account created!");
+                        showDialogue(context, "Account created!", "You can now log in");
+                      } else {
+                        showDialogue(context, "An error has occurred", "That email may already be taken");
+                      }
+                    });
+                    print("test");
+
+                  }
+                },
+                child: Center (child: Text('Sign up'),),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  showDialogue(BuildContext context, String titleText, String descText) {
+
+    // set up the button
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed:  () {
+        Navigator.of(context).pop(); // dismiss dialog
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(titleText),
+      content: Text(descText),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
+
