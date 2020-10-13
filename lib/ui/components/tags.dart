53f5154470d16db427465777906f3604b6594294
Patima Poochai <ppoochai@hawaii.edu>
2020-10-13 00:59:57 -1000

    Change tags to in-house component

---
+++ lib/ui/components/tags.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tagging/flutter_tagging.dart';
+
+
+class Tags extends StatefulWidget {
+  @override
+  _Tags createState() => _Tags();
+}
+
+class _Tags extends State<Tags> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("test");
+  }
+}
+
+/// Item Class
+class Interest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Item");
+  }
+}
--- lib/ui/pages/addproject.dart
+++ lib/ui/pages/addproject.dart
@@ -1,35 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tagging/flutter_tagging.dart';
 
 // components
 import '../components/appbar.dart';
-
-class Tag extends Taggable {
-  const Tag(this.name, this.position);
-
-  final String name;
-  final int position;
-
-  @override
-  List<Object> get props => [name, position];
-}
+import '../components/tags.dart';
 
 class AddProjectPage extends StatelessWidget {
-
-  static Future<List<Tag>> getLanguages(String query) async {
-    await Future.delayed(Duration(milliseconds: 500), null);
-    return <Tag>[
-      Tag(name: 'JavaScript', position: 1),
-      Tag(name: 'Python', position: 2),
-      Tag(name: 'Java', position: 3),
-      Tag(name: 'PHP', position: 4),
-      Tag(name: 'C#', position: 5),
-      Tag(name: 'C++', position: 6),
-    ].where((lang) => lang.name.toLowerCase().contains(query.toLowerCase())).toList();
-  }
-
-  List<Tag> _interests = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,38 +77,22 @@ class AddProjectPage extends StatelessWidget {
                 ),
               ),
             ),
-
-            FlutterTagging<Tag>(
-                initialItems: _interests,
-                findSuggestions: _suggestions,
-                configureChip: (lang) {
-                  return ChipConfiguration(
-                    label: Text(lang.name),
-                    backgroundColor: Colors.green,
-                    labelStyle: TextStyle(color: Colors.white),
-                    deleteIconColor: Colors.white,
-                  );
-                },
-                configureSuggestion: (lang) {
-                  return SuggestionConfiguration(
-                    title: Text(lang.name),
-                    subtitle: Text(lang.position.toString()),
-                    additionWidget: Chip(
-                      avatar: Icon(
-                        Icons.add_circle,
-                        color: Colors.white,
-                      ),
-                      label: Text('Add New Tag'),
-                      labelStyle: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w300,
-                      ),
-                      backgroundColor: Colors.green,
-                    ),
-                  );
-                },
-            )
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Interests',
+                style: TextStyle(
+                  fontSize: 22.0,
+                ),
+              ),
+            ),
+            Tags(),
           ]
       ),
     );
